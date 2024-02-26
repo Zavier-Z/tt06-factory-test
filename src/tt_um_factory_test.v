1e12e7c41669429a2778b3d2c3a494617bd03fef
Zavier-Z <160652977+Zavier-Z@users.noreply.github.com>
2024-02-26 15:35:59 -0500

    Update tt_um_factory_test.v

--- src/tt_um_factory_test.v
+++ src/tt_um_factory_test.v
@@ -1,41 +1,23 @@
-/*
- * tt_um_factory_test.v
- *
- * Test user module
- *
- * Author: Sylvain Munaut <tnt@246tNt.com>
- */
 
-`default_nettype none
+module johnson (clk, r, out);
 
-module tt_um_factory_test (
-	input  wire [7:0] ui_in,	// Dedicated inputs
-	output wire [7:0] uo_out,	// Dedicated outputs
-	input  wire [7:0] uio_in,	// IOs: Input path
-	output wire [7:0] uio_out,	// IOs: Output path
-	output wire [7:0] uio_oe,	// IOs: Enable path (active high: 0=input, 1=output)
-	input  wire       ena,
-	input  wire       clk,
-	input  wire       rst_n
-);
+parameter size=7;
+input clk;
+input r;
+output [0:size]out;
 
-	reg rst_n_i;
-	reg [7:0] cnt;
+reg [0:size]out;
 
-	always @(posedge clk or negedge rst_n)
-		if (~rst_n)
-			rst_n_i <= 1'b0;
+always @ (posedge clk or posedge r)
+	begin
+		if (r) 
+			out= 8'b0000_0000;
 		else
-			rst_n_i <= 1'b1;
+			out={~out[size],out[0:size-1]};   
+	end
 
-	always @(posedge clk or negedge rst_n_i)
-		if (~rst_n_i)
-			cnt <= 0;
-		else
-			cnt <= cnt + 1;
-	
-	assign uo_out  = ui_in[0] ? cnt : uio_in;
-	assign uio_out = ui_in[0] ? cnt : 8'h00;
-	assign uio_oe  = ui_in[0] ? 8'hff : 8'h00;
+endmodule
 
-endmodule // tt_um_factory_test
+
+
+
